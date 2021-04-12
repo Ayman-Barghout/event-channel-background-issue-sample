8bf5219917edd0fc6e4f0a046bb8eb9dc3e4b410
tirth-stasis <tirth@stasislabs.com>
2021-04-12 15:10:42 +0530

    add foreground service

--- android/src/main/kotlin/com/example/event_channel_background_issue/EventChannelBackgroundIssuePlugin.kt
+++ android/src/main/kotlin/com/example/event_channel_background_issue/EventChannelBackgroundIssuePlugin.kt
@@ -1,17 +1,27 @@
 package com.example.event_channel_background_issue
 
+import android.app.*
+import android.content.Intent
+import android.os.Build
 import android.os.Handler
+import android.os.IBinder
 import android.util.Log
 import androidx.annotation.NonNull
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.EventChannel
 import java.util.*
 
-class EventChannelBackgroundIssuePlugin : FlutterPlugin {
+class EventChannelBackgroundIssuePlugin : FlutterPlugin, ActivityAware, Service() {
 
+    private val tag: String = "<< From EventChannelBackgroundIssuePlugin >>"
     private var eventChannelOne: EventChannel? = null
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        Log.i(tag, "onAttachedToEngine called..")
         eventChannelOne = EventChannel(flutterPluginBinding.binaryMessenger, "event_channel_one")
         eventChannelOne?.setStreamHandler(object : EventChannel.StreamHandler {
             override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
@@ -23,7 +33,7 @@ class EventChannelBackgroundIssuePlugin : FlutterPlugin {
                             val randomNumber: Int = random.nextInt()
                             events.success(randomNumber.toString())
                             Log.i("sample-tag", "$randomNumber sent by EventSink")
-                            handler.postDelayed(this, 1000)
+                            handler.postDelayed(this, 2000)
                         }
                     }
                     handler.post(runnableCode)
@@ -36,6 +46,57 @@ class EventChannelBackgroundIssuePlugin : FlutterPlugin {
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        Log.i(tag, "onDetachedFromEngine called..")
+    }
 
+    override fun onBind(intent: Intent?): IBinder? {
+        Log.i(tag, "onBind called..")
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(tag, "onStartCommand called..")
+        return START_STICKY
+    }
+
+    override fun onCreate() {
+        Log.i(tag, "onCreate called..")
+        val notificationChannel = "my_notification_channel"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                    notificationChannel,
+                    "Dart In Background Notification Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager?.createNotificationChannel(serviceChannel)
+        }
+
+        val notification: Notification = NotificationCompat.Builder(this, notificationChannel)
+                .setContentTitle("My Foreground Service")
+                .setContentText("<-> My Foreground Service <->")
+                .setContentIntent(PendingIntent.getActivity(this,
+                        0, Intent(), 0))
+                .build()
+        startForeground(1, notification)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        Log.i(tag, "onAttachedToActivity called..")
+        ContextCompat.startForegroundService(
+                binding.activity,
+                Intent(binding.activity, EventChannelBackgroundIssuePlugin::class.java))
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        Log.i(tag, "onDetachedFromActivityForConfigChanges called..")
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        Log.i(tag, "onReattachedToActivityForConfigChanges called..")
+    }
+
+    override fun onDetachedFromActivity() {
+        Log.i(tag, "onDetachedFromActivity called..")
     }
 }
